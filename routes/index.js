4503e279a93a6726e8f45a814fa94b29437c7050
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-09-26 16:50:23 -0400

    Finished Homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Homepage' });
 });
+
+router.get('/about-me' , (req, res) => {
+  res.locals.title = 'About Me'
+  res.send('Hello world')
+})
 
 module.exports = router;
