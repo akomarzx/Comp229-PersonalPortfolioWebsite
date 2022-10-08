633eb928f6669020a41f7f28cfdf444d5b136b1f
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-10-08 12:04:58 -0400

    moved app config into config folder

--- config/app.js
+++ config/app.js
@@ -7,7 +7,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejsMate = require('ejs-mate');
-var indexRouter = require('./routes/index');
+var indexRouter = require('../routes/index');
 
 var app = express();
 
@@ -16,14 +16,14 @@ var app = express();
 // by avoiding the includes of 
 // header and footer partial in all views
 app.engine('ejs' , ejsMate);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 
